022f52693f368bbc50f7726eea11b8c93e5ab3e3
Trịnh Đắc Lượng <134267957+luonghacker1hit@users.noreply.github.com>
2024-07-25 13:15:39 +0700

    Bai_tap_bai_5

---
+++ Bai-5/ham.js
@@ -0,0 +1,36 @@
+// var x = 5
+// var b = 3
+// console.log("++x = ", ++x); 
+// console.log("x++ = ", x++); 
+// console.log("x = ", x);
+// console.log("--x = ", --x);
+// console.log("x-- = ", x--);
+// console.log("x = ", x);
+// console.log(2 !== '2'); // true
+// console.log(2 !== 2);
+
+// var  value1 = "one";
+// var value2 = "two" ;
+// console.log("value1 = " , value1);
+// console.log("value2 = " , value2);
+
+// var value3 = value1
+// value1 = value2
+// value2 = value3
+// console.log("value1 = " , value1);
+// console.log("value2 = " , value2);
+
+//Câu 6 :
+// function dien_tich_can_tim(r)
+// {
+//     dien_tich_hinh_vuong_to = 4 * (r ** 2);
+//     dien_tich_hinh_vuong_nho = 2 * (r ** 2);
+//     return dien_tich_hinh_vuong_to - dien_tich_hinh_vuong_nho;
+// }
+
+// var r = 7 ;
+// console.log("s = " , dien_tich_can_tim(r));
+
+
+
+
