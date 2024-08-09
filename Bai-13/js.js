d0278f37ebf66576f337dfde317b607ff3aceaa3
1337_DaKL <luongtrinh2k3ndad@gmail.com>
2024-08-09 13:28:14 +0700

    Bai 12

--- Bai-13/js.js
+++ Bai-13/js.js
@@ -1,56 +1,56 @@
-// localStorage.setItem("name", "Trịnh Đắc Lượng");
-// const ok = localStorage.getItem("name");
-// console.log(ok);
-// //localStorage.removeItem("name");
-// console.log(localStorage.key(0));
+localStorage.setItem("name", "Trịnh Đắc Lượng");
+const ok = localStorage.getItem("name");
+console.log(ok);
+//localStorage.removeItem("name");
+console.log(localStorage.key(0));
 
-// localStorage.setItem("light" , "");
+localStorage.setItem("light" , "");
 
-// const div = document.querySelector("#idv-local");
-// div.innerHTML = localStorage.getItem("name")  
-
-
-
-// const darkLight = localStorage.getItem("light");
-// if(darkLight)
-// {
-//     const body = document.querySelector("body");
-//     body.classList.toggle("dark");
-// }
-
-// const button  = document.querySelector("#dark");
-// button.addEventListener("click" , () => {
-//     const body = document.querySelector("body");
-//     body.classList.toggle("dark");
-
-// })
+const div = document.querySelector("#idv-local");
+div.innerHTML = localStorage.getItem("name")  
 
 
 
-// const   modeLocal = localStorage.getItem("mode");
-// if(modeLocal)
-// {
-//     const body = document.querySelector("body");
-//     body.classList.toggle(modeLocal);
-// }
+const darkLight = localStorage.getItem("light");
+if(darkLight)
+{
+    const body = document.querySelector("body");
+    body.classList.toggle("dark");
+}
+
+const button  = document.querySelector("#dark");
+button.addEventListener("click" , () => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark");
+
+})
 
 
-// const button = document.querySelector("#dark");
-// button.addEventListener("click" , () => {
-//     const body = document.querySelector("body");
-//     body.classList.toggle("dark");
+
+const   modeLocal = localStorage.getItem("mode");
+if(modeLocal)
+{
+    const body = document.querySelector("body");
+    body.classList.toggle(modeLocal);
+}
 
 
-//     const modeLocal = localStorage.getItem("mode");
-//     if(modeLocal)
-//     {
-//         localStorage.setItem("mode" , "");
-//     }
-//     else
-//     {
-//         localStorage.setItem("mode" , "dark");
-//     }
-// })
+const button = document.querySelector("#dark");
+button.addEventListener("click" , () => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark");
+
+
+    const modeLocal = localStorage.getItem("mode");
+    if(modeLocal)
+    {
+        localStorage.setItem("mode" , "");
+    }
+    else
+    {
+        localStorage.setItem("mode" , "dark");
+    }
+})
 
 
 
